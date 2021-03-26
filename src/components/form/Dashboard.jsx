8c46736be9cc8525214b1f154ad10d0309b6e561
Alessandra <itsalematos@hotmail.com>
2021-03-26 12:44:50 -0400

    Feat: name component styled and saving in state

--- src/components/form/Dashboard.jsx
+++ src/components/form/Dashboard.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import '../../styles/form/Form.css'
+import '../../styles/form/Dashboard.css'
+
+import graphic_plane from '../../images/graphic_plane.png'
+import purpleLogo from '../../images/purple_logo.png'
 
 function Dashboard({ nextStep }) {
 
   return (
     <div className="bodyForm">
-      <h3>Bem-Vindo!</h3>
-      <p>Curl up and sleep on the freshly laundered towels grass smells good but growl at dogs in my sleep. </p>
-      <button className="btn" onClick={nextStep} >Começar</button>
+      <div className="dashImg">
+        <img  src={graphic_plane} alt="aiplane alongside a growing graphic"/>
+        <img className="dashLogo" src={purpleLogo} alt="Trip Invest purple logo"/>
+      </div>
+      <div className="dashBg">
+        <h1 className="bold">Olá viajante!</h1>
+        <p className="dashText">Você já está a um passo</p>
+        <p className="dashText">para começar a fazer as malas.</p>
+        <button
+            name="start"
+            className="abledBtn startInvestBtn"
+            onClick={nextStep}
+          >
+            Vamos lá
+          </button>
+      </div> 
     </div>
   );
 }
--- src/components/form/NameForm.jsx
+++ src/components/form/NameForm.jsx
@@ -1,18 +1,52 @@
-import React from 'react';
-import '../../styles/form/Form.css'
-function NameForm({ nextStep }) {
+import React, { useState } from 'react';
+import '../../styles/form/Form.css';
+import '../../styles/form/NameForm.css';
+import { connect } from 'react-redux';
+import { travelerAction } from '../../actions/travelerAction';
+import travelersImg from '../../images/travelers.png'
+
+function NameForm({ nextStep, sendName }) {
+  const [isDisable, setIsDisable] = useState(true);
+  const [name, setName] = useState(true);
+
+  const handleInput = ({ target }) => {
+    const { name, value } = target
+    const payload = {
+      name,
+      value,
+    }
+    setName(payload)
+    setIsDisable(false);
+  }
+
+  const handleClick = () => {
+    sendName(name)
+    nextStep();
+  }
 
   return (
     <div className="bodyForm">
-      <span>Hey viajante!</span>
-      <br />
-      <span><strong>Como posso te chamar?</strong></span>
-      <br />
-      <input name="name" className="input" placeholder="Nome" />
-      <br />
-      <button className="btn" onClick={nextStep}>Próximo</button>
+      
+      <img className="travelers" src={travelersImg} alt="black woman and white man with luggage"/>
+      <p className="nameText">Mas antes do embarque,</p>
+      <p className=" nameText bold">como podemos te chamar?</p>
+      <input
+        name="name"
+        className="input nameInput"
+        placeholder="Nome"
+        onChange={handleInput}
+      />
+      <button
+        className={isDisable ? 'btn nextBtn' : 'abledBtn nextBtn'}
+        onClick={handleClick}>
+          Próximo
+      </button>
     </div>
   );
 }
 
-export default NameForm;
+const mapDispatchToProps = (dispatch) => ({
+  sendName: (e) => dispatch(travelerAction(e)),
+})
+
+export default connect(null, mapDispatchToProps)(NameForm);
